26ec6495f7aa41b7acdfb4e6c6cc256841dce2f5
iamaldrew <aldrenbobiles@gmail.com>
2019-10-15 20:31:33 +0800

    Added search result function for ldap

--- src/main/java/com/aldren/config/TestConfig.java
+++ src/main/java/com/aldren/config/TestConfig.java
@@ -5,8 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 public class TestConfig {
@@ -17,6 +23,21 @@ public class TestConfig {
     @Bean
     public String loadDirContext() throws NamingException {
         DirContext ctx = svc.ldapContext("riemann","password");
+
+        SearchControls searchControls = new SearchControls();
+        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        String searchFilter = "(&(objectClass=user)(sAMAccountName={0}))";
+
+        Set<String> roles = new HashSet<>();
+
+        NamingEnumeration<?> answer = ctx.search("dc=example,dc=com", searchFilter, new Object[]{"riemann"}, searchControls);
+
+        while(answer.hasMoreElements()) {
+            SearchResult sr = (SearchResult) answer.next();
+            Attributes attr = sr.getAttributes();
+
+            System.out.println("Attributes::" + attr.toString());
+        }
 
         return "";
     }
